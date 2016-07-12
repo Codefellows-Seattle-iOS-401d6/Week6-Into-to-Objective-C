08037db58221211d525905b5a97b7dc525092d4a
Rick Stoner <stonerrick19@gmail.com>
2016-07-12 12:50:46 -0700

    initial commit

---
+++ RickStoner/TuesdayLab/TuesdayLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TuesdayLab
+//
+//  Created by Rick  on 7/12/16.
+//  Copyright © 2016 Rick . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
