19fdfed18fae9ccef4fe72db9f8630bb5e0d06f8
sungkim3 <sng.kim103@gmail.com>
2016-07-12 13:08:55 -0700

    creates address project

---
+++ homework/AddressBook/AddressBook/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by Sung Kim on 7/12/16.
+//  Copyright © 2016 Sung Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
