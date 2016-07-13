ea81b513a780a093d4acacfc99c5c1ca18b60dfb
SEAN CHAMPAGNE <CHAMPAGNE.SEAN@GMAIL.COM>
2016-07-13 15:08:46 -0700

    committing wednesdays homework

---
+++ class-3/Week6-Day28/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week6-Day28
+//
+//  Created by Sean Champagne on 7/13/16.
+//  Copyright © 2016 Sean Champagne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ class-3/Week6-Day28/NSString+Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Extension.h
+//  Week6-Day28
+//
+//  Created by Sean Champagne on 7/13/16.
+//  Copyright © 2016 Sean Champagne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Extension)
+
++ (NSString *)archivePath;
+-(BOOL)isValidEmail;
+
+@end
