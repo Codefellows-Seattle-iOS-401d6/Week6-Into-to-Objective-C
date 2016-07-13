e14fe82c5e577930a26a393daf4d5d09c5d81435
Rick Stoner <stonerrick19@gmail.com>
2016-07-13 13:27:03 -0700

    added data model files student with getters and setters

---
+++ RickStoner/WednesdayObjCTablesLab/WednesdayObjCTablesLab/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WednesdayObjCTablesLab
+//
+//  Created by Rick  on 7/13/16.
+//  Copyright © 2016 Rick . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
